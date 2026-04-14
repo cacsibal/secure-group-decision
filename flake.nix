ff3db85de044c7db1ab7a4519db8bae759500380
Dashiell Elliott <dashiell.elliott@yahoo.com>
2026-04-13 21:32:27 -0400

    dev env setup

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Python encryption flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        (python3.withPackages (py-pkgs: with py-pkgs; [
+          fastecdsa
+          phe
+          python-lsp-server
+        ]))
+      ];
+    };
+  };
+}
